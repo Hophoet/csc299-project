4ad9800fcb69d42508df04e442163e64fd17b84d
hophoet <hophoet@gmail.com>
2020-10-01 17:27:37 +0000

    update tests

--- tests.py
+++ tests.py
@@ -22,10 +22,12 @@ def test_read_contact_return_200():
 def test_read_contact_return_404_with_correct_id_type():
     response = client.get("/contact/30000")
     assert response.status_code == 404
+    assert response.json() == {"detail":"contact not found"}
 
 def test_read_contact_return_404_wrong_id_type():
     response = client.get("/contact/8JKL")
     assert response.status_code == 404
+    assert response.json() == {"detail":"invalid contact id. contact id must be an integer  "}
 
 # COMPANIES TESTING
 def test_read_companies():
@@ -39,11 +41,12 @@ def test_read_company_return_200():
 def test_read_company_return_404_with_correct_id_type():
     response = client.get("/company/30000")
     assert response.status_code == 404
+    assert response.json() == {"detail":"company not found"}
 
 def test_read_company_return_404_wrong_id_type():
     response = client.get("/company/sfjk")
     assert response.status_code == 404
-
+    assert response.json() == {"detail":"invalid company id. contact id must be an integer  "}
 
 # ACTIVITY AREAS TESTING
 def test_read_activityareas():
@@ -57,10 +60,12 @@ def test_read_activityarea_return_200():
 def test_read_activityarea_return_404_with_correct_id_type():
     response = client.get("/activityarea/30000")
     assert response.status_code == 404
+    assert response.json() == {"detail":"activity area not found"}
 
 def test_read_activityarea_return_404_wrong_id_type():
     response = client.get("/activityarea/sfjk")
     assert response.status_code == 404
+    assert response.json() == {"detail":"invalid activity area id. contact id must be an integer  "}
 
 
 # SEARCH TESTING
@@ -79,3 +84,4 @@ def test_search_return_activityarea_with_200():
 def test_search_return_404():
     response = client.get('/search/?query=smdfkdj8484JDK')
     assert response.status_code == 404
+    assert response.json() == {"detail":"item not found"}
