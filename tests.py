6e060a9cfd2d2fcc4a9ef324cf155ce863769401
hophoet <hophoet@gmail.com>
2020-10-01 16:20:53 +0000

    add application tests

---
+++ tests.py
@@ -0,0 +1,81 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from main import app
+
+
+client = TestClient(app)
+
+#ROOT
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+
+# CONTACTS TESTING
+def test_read_contacts():
+    response = client.get("/contacts/")
+    assert response.status_code == 200
+
+def test_read_contact_return_200():
+    response = client.get("/contact/1")
+    assert response.status_code == 200
+
+def test_read_contact_return_404_with_correct_id_type():
+    response = client.get("/contact/30000")
+    assert response.status_code == 404
+
+def test_read_contact_return_404_wrong_id_type():
+    response = client.get("/contact/8JKL")
+    assert response.status_code == 404
+
+# COMPANIES TESTING
+def test_read_companies():
+    response = client.get("/companies/")
+    assert response.status_code == 200
+
+def test_read_company_return_200():
+    response = client.get("/company/1")
+    assert response.status_code == 200
+
+def test_read_company_return_404_with_correct_id_type():
+    response = client.get("/company/30000")
+    assert response.status_code == 404
+
+def test_read_company_return_404_wrong_id_type():
+    response = client.get("/company/sfjk")
+    assert response.status_code == 404
+
+
+# ACTIVITY AREAS TESTING
+def test_read_activityareas():
+    response = client.get("/activityareas/")
+    assert response.status_code == 200
+
+def test_read_activityarea_return_200():
+    response = client.get("/activityarea/1")
+    assert response.status_code == 200
+
+def test_read_activityarea_return_404_with_correct_id_type():
+    response = client.get("/activityarea/30000")
+    assert response.status_code == 404
+
+def test_read_activityarea_return_404_wrong_id_type():
+    response = client.get("/activityarea/sfjk")
+    assert response.status_code == 404
+
+
+# SEARCH TESTING
+def test_search_return_contact_with_200():
+    response = client.get('/search/?query=Alicia')
+    assert response.status_code == 200
+
+def test_search_return_company_with_200():
+    response = client.get('/search/?query=Technology')
+    assert response.status_code == 200
+
+def test_search_return_activityarea_with_200():
+    response = client.get('/search/?query=IPNET')
+    assert response.status_code == 200
+
+def test_search_return_404():
+    response = client.get('/search/?query=smdfkdj8484JDK')
+    assert response.status_code == 404
